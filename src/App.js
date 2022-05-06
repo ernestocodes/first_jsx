d004c3c460f04d8b6561f0f58f23f2b79158d521
ErnestoCarballo <ernestowcarballo12@gmail.com>
2022-05-06 16:19:43 -0700

    fourth commit

--- src/App.js
+++ src/App.js
@@ -33,11 +33,10 @@ function App() {
           })
         }
       </div>
-      <PersonCard firstName="Jane" lastName="Doe" age = {45} hairColor = "Black"/>
+      <PersonCardObj person={person1}/>
       <PersonCard firstName="John" lastName="Smith" age = {88} hairColor = "Brown"/>
       <PersonCard firstName="Millard" lastName="Fillmore" age = {50} hairColor = "Brown"/>
       <PersonCard firstName="Maria" lastName="Smith" age = {62} hairColor = "Brown"/>
-      <PersonCardObj person={person1}/>
     </div>
   );
 }
--- src/components/PersonCardObj.jsx
+++ src/components/PersonCardObj.jsx
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
 
 export class PersonCardObj extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            age: this.props.person.age
+        }
+    }
+
+    incrementAge = () => {
+        this.setState({age : this.state.age +1})
+    }
+
     render() {
-        const{firstName, lastName, age, hairColor} = this.props.person
+        const{firstName, lastName, hairColor} = this.props.person
+        const{age} = this.state
         return (
             <div>
                 <h2>{lastName}, {firstName}</h2>
                 <p>Age: {age}</p>
                 <p>Hair Color: {hairColor}</p>
+                <button onClick={ () => this.setState({age : this.state.age +1})}>Happy Birthday!</button>
+                <button onClick= {this.incrementAge}>Happy Birthday! too</button>
             </div>
         )
     }
