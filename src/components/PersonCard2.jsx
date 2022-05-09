b6c46d17651b6c0fe4f140aa28428d0409605741
ErnestoCarballo <ernestowcarballo12@gmail.com>
2022-05-09 15:22:59 -0700

    seventh commit

--- src/components/PersonCard2.jsx
+++ src/components/PersonCard2.jsx
@@ -4,12 +4,17 @@ import React, {useState} from 'react';
 const PersonCard2 = (props) => {
     const [age, setAge] = useState(props.age)
 
+    const birthday = () =>{
+        setAge(age+1);
+    }
+
     return(
         <div>
             <h1>{ props.lastName }, { props.firstName }</h1>
             <p>Age: { age }</p>
             <p>Hair Color: { props.hairColor }</p>
             <button onClick={ () => setAge(age+1)}>Happy Birthday!</button>
+            <button onClick={birthday}>Happy Birthday!</button>
         </div>
     );
 }
